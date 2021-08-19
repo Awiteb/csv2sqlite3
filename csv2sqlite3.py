302226f680876e2df0d2830069b7a4ce5f6e7793
Awiteb <59842932+Awiteb@users.noreply.github.com>
2021-08-19 19:53:33 +0300

    add single Quotations to table name

--- csv2sqlite3.py
+++ csv2sqlite3.py
@@ -19,19 +19,19 @@ def check_files(filenames: list, ex: str):
         pass
 
 def get_csv_as_dict(filename: Path) -> dict:
-    with open(filename) as csv_file:
+    with open(filename, encoding='utf-8') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for row in list(csv_reader):
             yield row
 
 def get_csv_columns(filename: Path) -> list:
-    keys = list([i for i in get_csv_as_dict(filename)][0].keys())
+    keys = list([str(key).strip() for key in [dct for dct in get_csv_as_dict(filename)][0].keys()])
     return keys
 
 def db_from_csv(filename: Path, cursor: sqlite3.Cursor) -> None:
     table = filename.name.strip('.csv')
     columns = get_csv_columns(filename)
-    sql_command = f"CREATE TABLE IF NOT EXISTS {table} ({make_join(columns)})"
+    sql_command = f"CREATE TABLE IF NOT EXISTS '{table}' ({make_join(columns)})"
     cursor.execute(sql_command)
     typer.echo(f"{Fore.YELLOW}N:{Fore.RESET} Table name: {table}")
     typer.echo(f"{Fore.YELLOW}N:{Fore.RESET} Columns: {columns}")
@@ -40,7 +40,9 @@ def db_from_csv(filename: Path, cursor: sqlite3.Cursor) -> None:
     for row in get_csv_as_dict(filename):
         counter += 1
         rows = list(row.values())
-        sql_command = f"INSERT INTO {table} VALUES ({make_join(rows)})"
+        while len(rows) > len(columns):
+            rows.pop()
+        sql_command = f"INSERT INTO '{table}' VALUES ({make_join(rows)})"
         cursor.execute(sql_command)
     typer.echo(f"{Fore.GREEN}N:{Fore.RESET} Done insert into {table} {counter} rows in {time()-start:.2f}s")
 
